9b1aec9d638829f20a2fab29e471e15111ad58f5
Deepikaarulraj <50837628+Deepikaarulraj@users.noreply.github.com>
2019-05-21 15:30:59 +0530

    21.05.2019/15:30

---
+++ Capitalise.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Capitalise{
+public static void main(String[] args) {
+String str;
+Scanner s = new Scanner(System.in);
+str = s.next();
+int n=str.length();
+char a[]=new char[n];
+a=String.toCharArray(str);
+int i;
+i=indexOf(" ");
+System.out.println();
+}
+}
+
+
