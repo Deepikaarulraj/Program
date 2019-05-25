109f3f3f5fc3923fb8a75880a014ad917c748ad0
Deepikaarulraj <50837628+Deepikaarulraj@users.noreply.github.com>
2019-05-25 09:13:52 +0530

    25.05.2019/09:12
    without using comparison operators and arithmetic operators.

---
+++ Checkifequal.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Checkifequal
+{
+ public static void main(String args[])
+  {
+    Scanner s = new Scanner(System.in);
+int f=s.nextInt();
+int l=s.nextInt();
+String s1=" "+f;
+String s2=" "+l;
+      System.out.println(s1.equals(s2));
+      
+       }
+}
+    
+    
