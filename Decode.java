b835b7bfac1c8f1afe457644d7d81c0ae231bf1c
Deepikaarulraj <50837628+Deepikaarulraj@users.noreply.github.com>
2019-06-06 16:23:07 +0530

    06.06.2019/04:22

---
+++ Decode.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Decode
+{
+ public static void main(String[] args)
+ {
+int temp;
+  Scanner s = new Scanner(System.in);
+   int n = s.nextInt();
+
+     int sum=11;
+  for(int i = 0;i<n;i++)  
+{
+         System.out.println(sum);
+	sum=sum*sum;
+         
+    }
+}
+ 
+} 
